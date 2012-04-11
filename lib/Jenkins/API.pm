b4042632de314adb1a026fbff583c07edef19bfa
Colin Newell <colin.newell@gmail.com>
2012-04-11 22:50:25 +0100

    Started to add config code

--- lib/Jenkins/API.pm
+++ lib/Jenkins/API.pm
@@ -46,6 +46,94 @@ sub create_job
     return $self->_client->responseCode() eq '200';
 }
 
+=head2 create_job_simple
+
+    $self->create_job_simple(
+        { 
+          description => "Project Description",
+          keepDependencies => "false",
+          scm =>
+          { 
+            configVersion => 2,
+            userRemoteConfigs =>
+            { 
+              'hudson.plugins.git.UserRemoteConfig' =>
+              { 
+                name => "",
+                refspec => {},
+                url => "ssh://git/....", 
+              }, 
+            },
+            branches => { 'hudson.plugins.git.BranchSpec' => { name => "master", }, },
+            disableSubmodules => "false",
+            recursiveSubmodules => "false",
+            doGenerateSubmoduleConfigurations => "false",
+            authorOrCommitter => "false",
+            clean => "false",
+            wipeOutWorkspace => "false",
+            pruneBranches => "false",
+            remotePoll => "false",
+            buildChooser =>
+            { class => "hudson.plugins.git.util.DefaultBuildChooser", },
+            gitTool => "Default",
+            submoduleCfg => { class => "list", },
+            relativeTargetDir => {},
+            reference => {},
+            excludedRegions => {},
+            excludedUsers => {},
+            gitConfigName => {},
+            gitConfigEmail => {},
+            skipTag => "false",
+            scmName => {},
+            class => "hudson.plugins.git.GitSCM", },
+          canRoam => "true",
+          disabled => "false",
+          blockBuildWhenDownstreamBuilding => "false",
+          blockBuildWhenUpstreamBuilding => "false",
+          triggers =>
+          { 
+            'hudson.triggers.SCMTrigger' =>
+            { 
+              spec => "*/5 * * * *", },
+            class => "vector", },
+          concurrentBuild => "false",
+          builders =>
+          { 
+            'hudson.tasks.Shell' =>
+            [ 
+            { command => "eval `/opt/perl5/bin/perl -Mlocal::lib=~/perl5`", }, 
+            { command => "/opt/perl5/bin/cpanm --installdeps . -l ~/perl5", }, 
+            { command => "/opt/perl5/bin/prove -I /var/lib/jenkins/perl5/lib/perl5/i686-linux -I /var/lib/jenkins/perl5/lib/perl5 --timer --formatter=TAP::Formatter::JUnit -l t > \${JOB_NAME}-\${BUILD_NUMBER}-junit.xml", }, 
+            ], },
+          publishers =>
+          { 
+            'hudson.tasks.junit.JUnitResultArchiver' =>
+            { 
+              testResults => "*junit.xml",
+              keepLongStdio => "false",
+              testDataPublishers => {}, },
+            'hudson.tasks.Mailer' =>
+            { 
+              recipients => "colin.newell",
+              dontNotifyEveryUnstableBuild => "false",
+              sendToIndividuals => "false", }, },
+          buildWrappers => {}, 
+        }
+    );
+
+=cut
+
+sub create_job_simple
+{
+    my ($self, $args) = @_;
+
+    my $schema = XML::Compile::Schema->new('compile.xsd');
+    my $doc = XML::LibXML::Document->new('1.0', 'UTF-8');
+    my $writer = $schema->compile(WRITER => 'project');
+    my $xml = $writer->($doc, $args);
+    $doc->setDocumentElement($xml);
+    $self->create_job($doc->toString());
+}
 
 =head1 AUTHOR
 
@@ -107,3 +195,4 @@ See http://dev.perl.org/licenses/ for more information.
 =cut
 
 1; # End of Jenkins::API
+
